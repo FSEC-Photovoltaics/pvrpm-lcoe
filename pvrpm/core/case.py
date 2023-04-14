fix npv calculation for ppa models

pvrpm/core/case.py
@@ -153,6 +153,7 @@ class SamCase:
         """
         Verifies loaded YAML configuration file.
         """
+
         # helper function to check distribution parameters
         def check_params(component: str, name: str, config: dict):
             if ck.MEAN not in config[ck.PARAM]:
@@ -608,7 +609,7 @@ class SamCase:
             pass
 
         try:
-            return np.array(self.output("cf_project_return_aftertax_npv")).sum()
+            return self.output("project_return_aftertax_npv")
         except AttributeError:
             pass
 